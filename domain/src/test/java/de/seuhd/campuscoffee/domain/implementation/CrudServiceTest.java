950efd648cebe5c0df9273d877c69b81cc50ba2a
Vahram Nersesyan <vahnersesyan@gmail.com>
2025-12-17 16:24:46 +0100

    Create a new file CrudServiceTest in the domain module that contains test cases for the CrudServiceImpl class

---
+++ domain/src/test/java/de/seuhd/campuscoffee/domain/implementation/CrudServiceTest.java
@@ -0,0 +1,9 @@
+package de.seuhd.campuscoffee.domain.implementation;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CrudServiceTest {
+    
+}
